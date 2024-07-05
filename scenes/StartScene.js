fda9dc5a3e5a398529309b1b57221cbaf608539d
Mika <mika28325@gmail.com>
2024-07-05 10:07:07 +0200

    Spiel fertig?!

--- scenes/StartScene.js
+++ scenes/StartScene.js
@@ -5,7 +5,7 @@ class StartScene extends Phaser.Scene {
 
 
     create() {
-        this.add.text(310, 280, 'Willkommen zu meinem Spiel!', { fontSize: '25px' });
+        this.add.text(170, 280, 'Willkommen zu Soldium, meinem ersten eigenem Spiel!', { fontSize: '25px' });
         this.add.text(140, 310, 'In dieser Welt kannst du frei herumlaufen und erkunden, ', { fontSize: '25px' });
         this.add.text(200, 340, 'was auch immer du willst. Nur pass auf, dass du', { fontSize: '25px' });
         this.add.text(270, 370, 'keinen Gegnern über den Weg läufst!', { fontSize: '25px' });
